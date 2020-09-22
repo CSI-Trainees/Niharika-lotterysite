f65d3a3267b61f1ab4af1a22eeb0fea5347a21d5
unknown <nv26112000@gmail.com>
2020-09-22 17:59:51 +0530

    initial commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function random()
+{
+
+
+        document.getElementById('no').innerHTML=(Math.floor(Math.random()* (30-20)) +20);
+        document.getElementById('no').disabled=true;
+
+        	}
