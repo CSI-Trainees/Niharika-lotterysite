03886bf06901306a93111cc2bae350b04e793968
unknown <nv26112000@gmail.com>
2020-09-24 03:12:02 +0530

    changes2

--- myscript.js
+++ myscript.js
@@ -6,3 +6,24 @@ function random()
         document.getElementById('no').disabled=true;
 
         	}
+function display()
+{
+	var x=document.getElementById("size").value ;
+	if(x==23)
+	{
+		document.getElementById("win").style.display = "block";
+	}
+	else
+		if(x==27)
+	{
+		document.getElementById("win2").style.display = "block";
+	}
+	else
+		if(x==20)
+	{
+		document.getElementById("win3").style.display = "block";
+	}
+	else
+		document.getElementById("lose").style.display = "block";
+
+};
