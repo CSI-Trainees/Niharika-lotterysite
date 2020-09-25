b3c0f4401fff9056e89a3233f898fc188821ec08
unknown <nv26112000@gmail.com>
2020-09-26 01:13:45 +0530

    final changes

--- myscript.js
+++ myscript.js
@@ -3,6 +3,7 @@ function random()
 
 
         document.getElementById('no').innerHTML=(Math.floor(Math.random()* (30-20)) +20);
+        document.getElementById('enter').style.display="block";
         document.getElementById('no').disabled=true;
 
         	}
